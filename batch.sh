123e30834f99635fe4fe58e994533c1f47f8ce7a
mihuber <mihuber@me.com>
2017-07-12 09:56:14 +0200

    script_dir as directory were script is saved

--- batch.sh
+++ batch.sh
@@ -1,59 +1,65 @@
 #!/bin/bash
 
-HCV-1a=("1000331007_S2_L001_R1_001.fastq.gz")
+HCV_1a=("1000278163-772_S3_L001_R1_001.fastq.gz
+1000361897-cs_S23_L001_R1_001.fastq.gz")
 
-HCV-1b=("")
+HCV_1b=("1000361646-G1b_S15_L001_R1_001.fastq.gz
+1000361646-G1b_S20_L001_R1_001.fastq.gz")
 
-HCV-1l=("")
+HCV_1l=("")
 
-HCV-3a=("")
+HCV_2c=("")
 
-HCV-4d=("")
+HCV_3a=("")
 
-HIV-1=("1000342682_S18_L001_R1_001.fastq.gz
-1000343615_S25_L001_R1_001.fastq.gz
-1000343652_S17_L001_R1_001.fastq.gz")
+HCV_4d=("")
 
-HIV-2=("")
+HIV_1=("")
 
+HIV_2=("")
 
-n=200000 # number of reads (default 50000)
-i=4     # iterations (default 4)
+HSV_1=("")
+
+n=200000 # number of reads (default 200000)
+i=4 # iterations (default 4)
+
 script_dir=$( dirname "$(readlink -f "$0")" )
+#script_dir="/home/huber.michael/Repositories/SmaltAlign/"
 
-
-for s in $HCV-1a; do
-	${script_dir}/smaltalign.sh -r ${script_dir}/References/HCV-1a.fasta -n $n -i $i $s
+for s in $HCV_1a; do
+    ${script_dir}/smaltalign.sh -r ${script_dir}/References/HCV-1a.fasta -n $n -i $i $s
 done
 
-
-for s in $HCV-1b; do
+for s in $HCV_1b; do
 	${script_dir}/smaltalign.sh -r ${script_dir}/References/HCV-1b.fasta -n $n -i $i $s
 done
 
-
-for s in $HCV-1l; do
+for s in $HCV_1l; do
 	${script_dir}/smaltalign.sh -r ${script_dir}/References/HCV-1l.fasta -n $n -i $i $s
 done
 
+for s in $HCV_2c; do
+	${script_dir}/smaltalign.sh -r ${script_dir}/References/HCV-2c.fasta -n $n -i $i $s
+done
 
-for s in $HCV-3a; do
+for s in $HCV_3a; do
 	${script_dir}/smaltalign.sh -r ${script_dir}/References/HCV-3a.fasta -n $n -i $i $s
 done
 
-
-for s in $HCV-4d; do
+for s in $HCV_4d; do
 	${script_dir}/smaltalign.sh -r ${script_dir}/References/HCV-4d.fasta -n $n -i $i $s
 done
 
-
-for s in $HIV-1; do
+for s in $HIV_1; do
 	${script_dir}/smaltalign.sh -r ${script_dir}/References/HIV-1.fasta -n $n -i $i $s
 done
 
-
-for s in $HIV-2; do
+for s in $HIV_2; do
 	${script_dir}/smaltalign.sh -r ${script_dir}/References/HIV-2.fasta -n $n -i $i $s
+done
+
+for s in $HSV_1; do
+	${script_dir}/smaltalign.sh -r ${script_dir}/References/HSV-1.fasta -n $n -i $i $s
 done
 
 
