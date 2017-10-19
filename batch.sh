a7d2b2b2afcdfe64c0bb30dd1c78e7a8c6b94dd1
sschmutz <stefan.schmutz@outlook.com>
2017-10-19 14:28:01 +0200

    rearange batch.sh and include execution of R scripts

--- batch.sh
+++ batch.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-HCV_1a=("1000278163-772_S3_L001_R1_001.fastq.gz
-1000361897-cs_S23_L001_R1_001.fastq.gz")
+# set script directory
+#script_dir=$(dirname "$(readlink -f "$0")")
+script_dir="/home/schmutz.stefan/Repositories/SmaltAlign/"
 
-HCV_1b=("1000361646-G1b_S15_L001_R1_001.fastq.gz
-1000361646-G1b_S20_L001_R1_001.fastq.gz")
+# set sample directory
+sample_dir=$(dirname "$(readlink -f "$0")")
+
+# set SmaltAlign parameters
+n=200000 # number of reads (default 200000)
+i=4 # iterations (default 4)
+
+# define Virus and subtype of the sequencing samples
+HCV_1a=("")
+
+HCV_1b=("")
 
 HCV_1l=("")
 
@@ -20,12 +30,7 @@ HIV_2=("")
 
 HSV_1=("")
 
-n=200000 # number of reads (default 200000)
-i=4 # iterations (default 4)
-
-script_dir=$( dirname "$(readlink -f "$0")" )
-#script_dir="/home/huber.michael/Repositories/SmaltAlign/"
-
+# run SmaltAlign for all defined samples
 for s in $HCV_1a; do
     ${script_dir}/smaltalign.sh -r ${script_dir}/References/HCV-1a.fasta -n $n -i $i $s
 done
@@ -62,6 +67,6 @@ for s in $HSV_1; do
 	${script_dir}/smaltalign.sh -r ${script_dir}/References/HSV-1.fasta -n $n -i $i $s
 done
 
-
-#Rscript ${script_dir}/cov_plot.R ./
-#Rscript ${script_dir}/wts.R ./
+# run R scripts
+Rscript ${script_dir}/cov_plot.R ${sample_dir}
+Rscript ${script_dir}/wts.R ${sample_dir}
