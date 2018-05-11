24d2f84b666349ceaf10cea8804ed9fa68861ad1
sschmutz <stefan.schmutz@outlook.com>
2018-05-11 14:10:58 +0200

    remove influenza from batch.sh

--- batch.sh
+++ batch.sh
@@ -35,47 +35,13 @@ HIV_2=("")
 
 HSV_1=("")
 
-IAV_seg1_H1N1=("")
-
-IAV_seg2_H1N1=("")
-
-IAV_seg3_H1N1=("")
-
-IAV_seg4_H1N1=("")
-
-IAV_seg5_H1N1=("")
-
-IAV_seg6_H1N1=("")
-
-IAV_seg7_H1N1=("")
-
-IAV_seg8_H1N1=("")
-
-IAV_seg1_H3N2=("")
-
-IAV_seg2_H3N2=("")
-
-IAV_seg3_H3N2=("")
-
-IAV_seg4_H3N2=("")
-
-IAV_seg5_H3N2=("")
-
-IAV_seg6_H3N2=("")
-
-IAV_seg7_H3N2=("")
-
-IAV_seg8_H3N2=("")
-
 # define an array containing all Viruses
 viruses=(HCV_1a HCV_1b HCV_1h HCV_1l
          HCV_2c
          HCV_3a HCV_3b
          HCV_4d HCV_4f
          HIV_1 HIV_2
-         HSV_1
-         IAV_seg1_H1N1 IAV_seg2_H1N1 IAV_seg3_H1N1 IAV_seg4_H1N1 IAV_seg5_H1N1 IAV_seg6_H1N1 IAV_seg7_H1N1 IAV_seg8_H1N1
-         IAV_seg1_H3N2 IAV_seg2_H3N2 IAV_seg3_H3N2 IAV_seg4_H3N2 IAV_seg5_H3N2 IAV_seg6_H3N2 IAV_seg7_H3N2 IAV_seg8_H3N2)
+         HSV_1)
 
 # run SmaltAlign for all defined samples
 for virus in "${viruses[@]}"
